03d143a9c6eaea6e3200949e8666aee8a5a1c9e1
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-06-20 21:46:27 +0200

    Additional tag "Billing-Service" in cdk

--- cdk/bin/amy-email-worker.ts
+++ cdk/bin/amy-email-worker.ts
@@ -7,14 +7,16 @@ import { CronStack } from '../lib/cron-stack';
 
 const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
 
-const APPLICATION = 'amy-email-worker';
+const APPLICATION_TAG = 'amy-email-worker';
+const BILLING_SERVICE_TAG = 'AMY';
 const STAGE = 'staging';
 const app = new App();
 
 const vpcStack = new VpcStack(app, 'EmailWorkerVpc', {
   env: env
 });
-Tags.of(vpcStack).add('ApplicationID', APPLICATION);
+Tags.of(vpcStack).add('ApplicationID', APPLICATION_TAG);
+Tags.of(vpcStack).add('Billing-Service', BILLING_SERVICE_TAG);
 Tags.of(vpcStack).add('Environment', STAGE);
 
 const lambdaStack = new LambdaStack(app, 'EmailWorkerLambda', {
@@ -22,12 +24,14 @@ const lambdaStack = new LambdaStack(app, 'EmailWorkerLambda', {
   vpc: vpcStack.vpc,
   stage: STAGE,
 });
-Tags.of(lambdaStack).add('ApplicationID', APPLICATION);
+Tags.of(lambdaStack).add('ApplicationID', APPLICATION_TAG);
+Tags.of(lambdaStack).add('Billing-Service', BILLING_SERVICE_TAG);
 Tags.of(lambdaStack).add('Environment', STAGE);
 
 const cronStack = new CronStack(app, 'EmailWorkerCron', {
   env: env,
   lambdaFunction: lambdaStack.lambdaFunction,
 });
-Tags.of(cronStack).add('ApplicationID', APPLICATION);
+Tags.of(cronStack).add('ApplicationID', APPLICATION_TAG);
+Tags.of(cronStack).add('Billing-Service', BILLING_SERVICE_TAG);
 Tags.of(cronStack).add('Environment', STAGE);
