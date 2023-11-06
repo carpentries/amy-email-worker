32deaefe5eb2d1074b5e25cecda24fa8ff8c7840
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-11-06 22:38:15 +0100

    [#6] Use email-worker-specific SSM params for mailgun auth

--- worker/utils/settings.py
+++ worker/utils/settings.py
@@ -12,9 +12,16 @@ def read_settings_from_env() -> Settings:
 
 
 def read_mailgun_credentials(stage: str) -> MailgunCredentials:
-    api_key_parameter = read_ssm_parameter(f"/{stage}/amy/mailgun_key")
+    api_key_parameter = read_ssm_parameter(f"/{stage}/email-worker/mailgun_key")
     api_key = get_parameter_value(api_key_parameter) if api_key_parameter else "fakeKey"
+
+    sender_domain_parameter = read_ssm_parameter(
+        f"/{stage}/email-worker/mailgun_sender_domain"
+    )
+    sender_domain = (
+        get_parameter_value(sender_domain_parameter) if sender_domain_parameter else ""
+    )
     return MailgunCredentials(
-        MAILGUN_SENDER_DOMAIN=os.getenv("MAILGUN_SENDER_DOMAIN") or "",
+        MAILGUN_SENDER_DOMAIN=sender_domain,
         MAILGUN_API_KEY=api_key,
     )
