73f9f8d30319d1a567e70349e4e866b3b6f8dd17
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2024-02-11 12:45:24 +0100

    [#10] Update todos

--- worker/main.py
+++ worker/main.py
@@ -27,10 +27,12 @@ logger.setLevel(logging.INFO)  # use logging.DEBUG to see boto3 logs
 # 3. add mapping for API endpoints ✅
 # 4. fetch and render emails ✅
 # 5. write unit tests! ✅
-# 6. create new simplified API in AMY
-# 7. create schemas for the endpoints
-# 8. add endpoints for managing emails
-# 9. rewrite email logic from handler below to use the new email endpoints
+# 6. create new simplified API in AMY ✅
+# 7. use authentication in the new API ✅
+# 8. use authentication in the worker
+# 9. create schemas for the endpoints
+# 10. add endpoints for managing emails
+# 11. rewrite email logic from handler below to use the new email endpoints
 
 
 async def main(event: dict[Any, Any], context: LambdaContext) -> WorkerOutput:
