f9a9f9aae299cdcb790e2a1f03e03504c2ef904c
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2024-02-18 18:09:07 +0100

    [#10] Update TODOs

--- worker/main.py
+++ worker/main.py
@@ -13,7 +13,7 @@ from src.database import (
     read_database_credentials_from_ssm,
 )
 from src.handler import handle_email
-from src.settings import STAGE, SETTINGS, read_mailgun_credentials
+from src.settings import SETTINGS, STAGE, read_mailgun_credentials
 from src.token import CachedToken
 from src.types import WorkerOutput
 
@@ -31,9 +31,11 @@ logger.setLevel(logging.INFO)  # use logging.DEBUG to see boto3 logs
 # 6. create new simplified API in AMY ✅
 # 7. use authentication in the new API ✅
 # 8. use authentication in the worker
-# 9. create schemas for the endpoints
-# 10. add endpoints for managing emails
-# 11. rewrite email logic from handler below to use the new email endpoints
+# 9. limit token generation only for accounts with special permission
+# 10. update CDK with envvars / secrets
+# 11. create schemas for the endpoints
+# 12. add endpoints for managing emails
+# 13. rewrite email logic from handler below to use the new email endpoints
 
 
 async def main(event: dict[Any, Any], context: LambdaContext) -> WorkerOutput:
