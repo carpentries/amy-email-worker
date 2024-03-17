3ee0d3093bd43130020605edbcb47ab814963cbc
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2024-03-17 22:17:51 +0100

    [#10] Switch from database access to rest API access

--- worker/src/api.py
+++ worker/src/api.py
@@ -1,18 +1,110 @@
 import asyncio
 import logging
+from dataclasses import dataclass
 from typing import Any, Callable, cast
 from urllib.parse import ParseResult, urlparse
+from uuid import UUID
 
 import httpx
 
 from src.settings import SETTINGS
-from src.types import AuthToken, BasicTypes
+from src.token import TokenCache
+from src.types import AuthToken, BasicTypes, ScheduledEmail
 
 logger = logging.getLogger("amy-email-worker")
 
 
 class UriError(Exception):
     pass
+
+
+@dataclass
+class ScheduledEmailController:
+    api_base_url: str
+    client: httpx.AsyncClient
+    token_cache: TokenCache
+
+    def auth_headers(self, token: str) -> dict[str, str]:
+        return {"Authorization": f"Token {token}"}
+
+    async def get_by_id(self, id_: UUID) -> ScheduledEmail:
+        token = await self.token_cache.get_token()
+        headers = self.auth_headers(token.token)
+        result = await self.client.get(
+            f"{self.api_base_url}/v2/scheduledemail/{id_}", headers=headers
+        )
+        result.raise_for_status()
+        return ScheduledEmail(**result.json())
+
+    async def get_paginated(
+        self, url: str, *, max_pages: int = 10
+    ) -> list[dict[str, Any]]:
+        """Paginate over results and collect them. Safety break at max_pages.
+
+        Param `url` should contain `{}` for page number, indexation starts from 1 and
+        increments by 1.
+        """
+        results: list[dict[str, Any]] = []
+
+        token = await self.token_cache.get_token()
+        headers = self.auth_headers(token.token)
+
+        # safety break, preventing infinite loop
+        counter = 0
+        while counter < max_pages:
+            result = await self.client.get(url.format(counter + 1), headers=headers)
+
+            # Could be 404 if pagination is out of range
+            if result.status_code != 200:
+                break
+
+            counter += 1
+            results.extend(result.json()["results"])
+
+        return results
+
+    async def get_all(self) -> list[ScheduledEmail]:
+        url = f"{self.api_base_url}/v2/scheduledemail?page={{}}"
+        results = await self.get_paginated(url)
+        scheduled_emails = [ScheduledEmail(**result) for result in results]
+        return scheduled_emails
+
+    async def get_scheduled_to_run(self) -> list[ScheduledEmail]:
+        url = f"{self.api_base_url}/v2/scheduledemail/scheduled_to_run?page={{}}"
+        results = await self.get_paginated(url)
+        scheduled_emails = [ScheduledEmail(**result) for result in results]
+        return scheduled_emails
+
+    async def lock_by_id(self, id_: UUID) -> ScheduledEmail:
+        token = await self.token_cache.get_token()
+        headers = self.auth_headers(token.token)
+        result = await self.client.post(
+            f"{self.api_base_url}/v2/scheduledemail/lock/{id_}", headers=headers
+        )
+        result.raise_for_status()
+        return ScheduledEmail(**result.json())
+
+    async def fail_by_id(self, id_: UUID, details: str) -> ScheduledEmail:
+        token = await self.token_cache.get_token()
+        headers = self.auth_headers(token.token)
+        result = await self.client.post(
+            f"{self.api_base_url}/v2/scheduledemail/fail/{id_}",
+            json={"details": details},
+            headers=headers,
+        )
+        result.raise_for_status()
+        return ScheduledEmail(**result.json())
+
+    async def succeed_by_id(self, id_: UUID, details: str) -> ScheduledEmail:
+        token = await self.token_cache.get_token()
+        headers = self.auth_headers(token.token)
+        result = await self.client.post(
+            f"{self.api_base_url}/v2/scheduledemail/succeed/{id_}",
+            json={"details": details},
+            headers=headers,
+        )
+        result.raise_for_status()
+        return ScheduledEmail(**result.json())
 
 
 def map_api_uri_to_url(api_uri: str) -> str:
