c47b8c62486f5813ac3960a4ba2500409b8fb4ff
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2024-08-31 21:50:53 +0200

    [#21] Create stacks for production, use applyStandardTags

--- cdk/bin/amy-email-worker.ts
+++ cdk/bin/amy-email-worker.ts
@@ -1,39 +1,82 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import { App, Tags } from 'aws-cdk-lib';
+import { App } from 'aws-cdk-lib';
 import { VpcStack } from '../lib/vpc-stack';
 import { LambdaStack } from '../lib/lambda-stack';
 import { CronStack } from '../lib/cron-stack';
+import { Settings, StandardTags } from '../lib/types';
+import { applyStandardTags } from '../lib/utils';
 
-const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
+const stagingSettings: Settings = {
+  stage: 'staging',
+  apiBaseUrl: 'https://test-amy2.carpentries.org/api',
+};
 
-const APPLICATION_TAG = 'amy-email-worker';
-const BILLING_SERVICE_TAG = 'AMY';
-const STAGE = 'staging';
-const API_BASE_URL = 'https://test-amy2.carpentries.org/api'
+const stagingTags: StandardTags = {
+  applicationTag: 'amy-email-worker',
+  billingServiceTag: 'AMY',
+  stage: stagingSettings.stage,
+};
+
+const productionSettings: Settings = {
+  stage: 'production',
+  apiBaseUrl: 'https://amy.carpentries.org/api',
+};
+
+const productionTags: StandardTags = {
+  applicationTag: 'amy-email-worker',
+  billingServiceTag: 'AMY',
+  stage: productionSettings.stage,
+};
+
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
 const app = new App();
 
-const vpcStack = new VpcStack(app, 'EmailWorkerVpc', {
+/*******************/
+/** S T A G I N G **/
+/*******************/
+const vpcStackStaging = new VpcStack(app, 'EmailWorkerVpc', {
   env: env
 });
-Tags.of(vpcStack).add('ApplicationID', APPLICATION_TAG);
-Tags.of(vpcStack).add('Billing-Service', BILLING_SERVICE_TAG);
-Tags.of(vpcStack).add('Environment', STAGE);
+applyStandardTags(vpcStackStaging, stagingTags);
 
-const lambdaStack = new LambdaStack(app, 'EmailWorkerLambda', {
+const lambdaStackStaging = new LambdaStack(app, 'EmailWorkerLambda', {
   env: env,
-  vpc: vpcStack.vpc,
-  stage: STAGE,
-  api_base_url: API_BASE_URL,
+  vpc: vpcStackStaging.vpc,
+  stage: stagingSettings.stage,
+  api_base_url: stagingSettings.apiBaseUrl,
 });
-Tags.of(lambdaStack).add('ApplicationID', APPLICATION_TAG);
-Tags.of(lambdaStack).add('Billing-Service', BILLING_SERVICE_TAG);
-Tags.of(lambdaStack).add('Environment', STAGE);
+applyStandardTags(lambdaStackStaging, stagingTags);
 
-const cronStack = new CronStack(app, 'EmailWorkerCron', {
+const cronStackStaging = new CronStack(app, 'EmailWorkerCron', {
   env: env,
-  lambdaFunction: lambdaStack.lambdaFunction,
+  lambdaFunction: lambdaStackStaging.lambdaFunction,
 });
-Tags.of(cronStack).add('ApplicationID', APPLICATION_TAG);
-Tags.of(cronStack).add('Billing-Service', BILLING_SERVICE_TAG);
-Tags.of(cronStack).add('Environment', STAGE);
+applyStandardTags(cronStackStaging, stagingTags);
+
+
+/*************************/
+/** P R O D U C T I O N **/
+/*************************/
+const vpcStackProduction = new VpcStack(app, 'EmailWorkerVpcProd', {
+  env: env
+});
+applyStandardTags(vpcStackProduction, productionTags);
+
+const lambdaStackProduction = new LambdaStack(app, 'EmailWorkerLambdaProd', {
+  env: env,
+  vpc: vpcStackProduction.vpc,
+  stage: productionSettings.stage,
+  api_base_url: productionSettings.apiBaseUrl,
+});
+applyStandardTags(lambdaStackProduction, productionTags);
+
+const cronStackProduction = new CronStack(app, 'EmailWorkerCronProd', {
+  env: env,
+  lambdaFunction: lambdaStackProduction.lambdaFunction,
+});
+applyStandardTags(cronStackProduction, productionTags);
